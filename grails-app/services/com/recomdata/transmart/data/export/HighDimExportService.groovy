2f118b34efcaa295c15894db9616a3eddbb0b57f
Shicai Wang <evanswang@ymail.com>
2015-11-13 15:15:20 +0000

    kv init

--- grails-app/services/com/recomdata/transmart/data/export/HighDimExportService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/HighDimExportService.groovy
@@ -1,18 +1,28 @@
 package com.recomdata.transmart.data.export
 
+import org.transmart.db.dataquery.mrna.AnnotationRecord
 import org.transmartproject.core.dataquery.DataRow
 import org.transmartproject.core.dataquery.TabularResult
 import org.transmartproject.core.dataquery.highdim.AssayColumn
 import org.transmartproject.core.dataquery.highdim.HighDimensionDataTypeResource
 import org.transmartproject.core.dataquery.highdim.assayconstraints.AssayConstraint
 import org.transmartproject.core.dataquery.highdim.projections.Projection
+import org.transmartproject.core.ontology.OntologyTerm
 import org.transmartproject.export.HighDimExporter
+
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import org.transmart.db.dataquery.mrna.ExpressionRecord
+import org.transmart.db.dataquery.vcf.KVVcfModule
+import org.transmartproject.export.HighDimExporter
+import org.transmart.db.dataquery.SQLModule
+import org.transmart.db.dataquery.mrna.KVMrnaModule
 
 class HighDimExportService {
 
     def highDimensionResourceService
     def highDimExporterRegistry
     def queriesResourceService
+    def conceptsResourceService
 
     // FIXME: jobResultsService lives in Rmodules, so this is probably not a dependency we should have here
     def jobResultsService
@@ -27,14 +37,23 @@ class HighDimExportService {
      * - args.jobName                 - name of the current export job to check status whether we need to break export.
      */
     def exportHighDimData(Map args) {
+
         Long resultInstanceId = args.resultInstanceId as Long
         List<String> conceptKeys = args.conceptKeys
+
+        String jobName = args.jobName
+        String dataType = args.dataType
+        List<String> conceptPaths = args.conceptPaths
+        String studyDir = args.studyDir
+        Collection<String> gplIds = args.gplIds
+        String format = args.format
 
         if (jobIsCancelled(args.jobName)) {
             return null
         }
 
         def fileNames = []
+
         HighDimensionDataTypeResource dataTypeResource = highDimensionResourceService.getSubResourceForType(args.dataType)
         if (!conceptKeys) {
             def queryResult = queriesResourceService.getQueryResultFromId(resultInstanceId)
@@ -79,22 +98,106 @@ class HighDimExportService {
         Projection projection = dataTypeResource.createProjection(exporter.projection)
 
         // Retrieve the data itself
-        TabularResult<AssayColumn, DataRow<Map<String, String>>> tabularResult =
-                dataTypeResource.retrieveData(assayConstraints, [], projection)
 
-        File outputFile = new File(studyDir,
-                "${dataType}_${makeFileNameFromConceptPath(conceptPath)}_${index}.${format.toLowerCase()}")
+        // Start exporting
+        // @wsc graft key value on to the relational function here for mrna and vcf format
+        // check the mrna output format, write down different types of data,
+        //File outputFile = new File(studyDir, dataType + '.' + format.toLowerCase())
+        //String fileName = outputFile.getAbsolutePath()
 
-        try {
-            outputFile.withOutputStream { outputStream ->
-                exporter.export tabularResult, projection, outputStream, { jobIsCancelled(jobName) }
+
+        if(ConfigurationHolder.config.org.transmart.kv.enable && dataType.equals("mrna")) { List<BigDecimal> patientList = SQLModule.getPatients(resultInstanceId + "")
+
+            Map<String, String> study_concecpt = SQLModule.getTrialandConceptCD(term.getFullName())
+            String studyName = study_concecpt.get("study_name")
+            String conceptCD = study_concecpt.get("concept_cd")
+            Map<BigDecimal, AnnotationRecord> patientMap = SQLModule.getPatientMapping(patientList, conceptCD)
+            final String COL_FAMILY_RAW = "raw"
+            final String COL_FAMILY_LOG = "log"
+            final String COL_FAMILY_ZSCORE = "zscore"
+
+            KVMrnaModule kvMrnaModule = new KVMrnaModule("microarray", "raw")
+            PrintWriter pw = null
+            long t1 = System.currentTimeMillis()
+            int count = 0
+            try {
+                File nodeDataFolder = new File(studyDir, getRelativeFolderPathForSingleNode(term))
+                File outputFile = new File(studyDir,
+                        "${dataType}_${makeFileNameFromConceptPath(conceptPath)}_${index}.${format.toLowerCase()}")
+                if (outputFile.exists()) {
+                    throw new RuntimeException("${outputFile} file already exists.")
+                }
+                nodeDataFolder.mkdirs()
+                //outputFiles << outputFile
+
+                pw = new PrintWriter(outputFile)
+                pw.println("PATIENT ID\tSAMPLE TYPE\tTIMEPOINT\tTISSUE TYPE\tGPL ID\tASSAY ID\tSAMPLE CODE\tTRIALNAME\tVALUE\tLOG2E\tZSCORE\tPROBE\tGENE ID\tGENE SYMBO")
+                Map<String, List<ExpressionRecord>> resultsMap = new HashMap<String, List<ExpressionRecord>>()
+                int i = 0
+                patientMap.keySet().each { patientID ->
+                    count ++;
+                    resultsMap.put(patientID, kvMrnaModule.getAllRecords(studyName, patientID, conceptCD))
+                    i++
+                    if (i >= 20) {
+                        resultsMap.keySet().each { paID ->
+                            AnnotationRecord annoRecord = patientMap.get(paID)
+                            resultsMap.get(paID).each { record ->
+                                pw.println(annoRecord.getSubjectID() + "\t" +
+                                        annoRecord.getSampleType() + "\t" +
+                                        annoRecord.getTimePoint() + "\t" +
+                                        annoRecord.getTissueType() + "\t" +
+                                        annoRecord.getGplID() + "\t" +
+                                        annoRecord.getAssayID() + "\t" +
+                                        annoRecord.getSampleCD() + "\t" +
+                                        studyName + "\t" +
+                                        record.getValues().get(COL_FAMILY_RAW) + "\t" +
+                                        record.getValues().get(COL_FAMILY_LOG) + "\t" +
+                                        record.getValues().get(COL_FAMILY_ZSCORE) + "\t" +
+                                        record.getProbeset() + "\t" +
+                                        record.getGene() + "\t" +
+                                        "null")
+                            }
+                        }
+                        resultsMap.clear()
+                        i = 0
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace()
+            } finally {
+                pw.close()
+                long t2 = System.currentTimeMillis()
             }
-        } catch (RuntimeException e) {
-            log.error('Data export to the file has thrown an exception', e)
-        } finally {
-            tabularResult.close()
-        }
+        } else if (ConfigurationHolder.config.org.transmart.kv.enable && dataType.equals("vcf")) {
+            List<BigDecimal> patientList = SQLModule.getPatients(resultInstanceId)
+            Map<BigDecimal, String> patientMap = SQLModule.getPatientMap(patientList)
+            PrintWriter pw = null
+            KVVcfModule kvVcfModule = new KVVcfModule("vcf-subject.index");
+            try {
+                pw = new PrintWriter(outputFile)
+                pw.println("TRIAL NAME\tPATIENT ID\tCHROMOSOME\tPOSITION\tASSAYID\tGPLID\tREFERENCE\tREFERENCEALLELE\tRSID\tSAMPLECODE\tSAMPLETYPE\tTIMEPOINT\tTISSUETYPE\tVARIANT\tVARIANTTYPE")
+                patientMap.keySet().each { patientID ->
+                    kvVcfModule.writeAllRecords(pw, patientMap.get(patientID), patientID)
+                }
+            } catch (IOException e) {
+                e.printStackTrace()
+            } finally {
+                pw.close()
+            }
+        } else {
+            TabularResult<AssayColumn, DataRow<Map<String, String>>> tabularResult =
+                    dataTypeResource.retrieveData(assayConstraints, [], projection)
 
+            try {
+                outputFile.withOutputStream { outputStream ->
+                    exporter.export tabularResult, projection, outputStream, { jobIsCancelled(jobName) }
+                }
+            } catch (RuntimeException e) {
+                log.error('Data export to the file has thrown an exception', e)
+            } finally {
+                tabularResult.close()
+            }
+        }
         outputFile
     }
 
